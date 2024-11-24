9e297da6fecf1299944462060db094fc6e563ed6
Hyeonjae Nam <jaemuon5582@gmail.com>
2024-11-24 22:16:18 +0900

    Initial commit

---
+++ src/main/java/moe/fotone/hatsuboshibackstage/HatsuboshiBackstageApplication.java
@@ -0,0 +1,13 @@
+package moe.fotone.hatsuboshibackstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HatsuboshiBackstageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HatsuboshiBackstageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/moe/fotone/hatsuboshibackstage/HatsuboshiBackstageApplicationTests.java
@@ -0,0 +1,13 @@
+package moe.fotone.hatsuboshibackstage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HatsuboshiBackstageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
